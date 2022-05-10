75694eb347a30e13c5198fbdb4d092d56527456d
Jordan Azoulay <azoulay.jordan@gmail.com>
2022-05-10 10:21:55 +0200

    avancement

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,15 @@
-import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import React, { useEffect } from "react";
+import gameObjects from "./GameDevSoftware/gameObjects.json";
 
 function App() {
+  useEffect(() => {
+    gameObjects.forEach((o) => {
+      console.log(require(`./GameDevSoftware/gameObjects/${o.value}`));
+    });
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <p>Hello world</p>
     </div>
   );
 }
