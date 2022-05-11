0f2666fba1933793d54e735ae63aafa43f3b126c
Jordan Azoulay <azoulay.jordan@gmail.com>
2022-05-11 17:02:44 +0200

    avancement

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,31 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import objects from "./GameDevSoftware/gameObjects/index.json";
+
+const gameObjects: any = [];
+
+objects.forEach((o) => {
+  gameObjects.push(require(`./GameDevSoftware/gameObjects/${o}.json`));
+});
 
 function App() {
+  const [firstObject, setFirstObject] = useState<any>();
+  useEffect(() => {
+    setFirstObject(gameObjects[0]);
+  }, []);
+  const findUrlImg = (src: string | { src: string; module: string }) => {
+    const assetsUrl = "assets/";
+    if (typeof src === "string") {
+      return `${assetsUrl}images/${src}`;
+    }
+    return `${assetsUrl}${src.module}/images/${src.src}`;
+  };
+  console.log("🚀 ~ file: App.tsx ~ line 24 ~ App ~ gameObjects", gameObjects);
+  console.log("🚀 ~ file: App.tsx ~ line 26 ~ App ~ firstObject", firstObject);
   return (
     <div>
       <p>Hello world</p>
+      {firstObject && <img src={findUrlImg(firstObject.image)} alt="" />}
+      {/* <img src="assets/darkbluedungeon/images/amulet.png" alt="" /> */}
     </div>
   );
 }
