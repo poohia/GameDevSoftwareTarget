4a7f40ee699d0eaebcbea32913f1c6bca5c17d55
Jordan AZOULAY <jazoulay@joazco.com>
2026-03-29 12:43:01 +0200

    avancement

--- src/GameDevSoftware/modules/GDSTModule/hooks/useItem.ts
+++ src/GameDevSoftware/modules/GDSTModule/hooks/useItem.ts
@@ -2,14 +2,19 @@ import { useCallback } from "react";
 
 import { useGameObjects } from "../../../../hooks";
 import { Item } from "../types";
+import { useGameProvider } from "../../../../gameProvider";
 
 const useItem = () => {
+  const { getValueFromConstant } = useGameProvider();
   const { getGameObjectsFromId, getGameObjectsFromType } = useGameObjects();
 
-  const getItemsByUniqueKey = useCallback((uniqKey: string) => {
-    return getGameObjectsFromType<Item>("item").filter(
-      (go) => go.uniqKey === uniqKey
-    );
+  const getItemsByUniqueKey = useCallback((uniqueKey: string) => {
+    return getGameObjectsFromType<Item>("item").filter((go) => {
+      if (go.uniqueKey.startsWith("@c:")) {
+        return getValueFromConstant(uniqueKey) === uniqueKey;
+      }
+      return go.uniqueKey === uniqueKey;
+    });
   }, []);
 
   return {
