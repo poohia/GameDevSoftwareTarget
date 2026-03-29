69af9318f6dbdb7183c2fbba56c149090cf14e94
Jordan AZOULAY <jazoulay@joazco.com>
2026-03-29 12:38:23 +0200

    avancement

---
+++ src/GameDevSoftware/modules/GDSTModule/types.ts
@@ -0,0 +1,10 @@
+export type Item = {
+  _id: number;
+  _title: string;
+  uniqKey: string;
+  name: string;
+  texts: string[];
+  images: string[];
+  gameObjectTarget: string;
+  order: number;
+};
